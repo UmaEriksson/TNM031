46e6b7bc452536ab2852a9ba4524e0d72946a639
Hugo <hugodahl99@hotmail.com>
2022-09-20 15:02:52 +0200

    beginning to revamp, strings as messages

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String args[]) {
         int p, q, n, z, d = 0, e, i;
 
+        String message = "Hello world";
+
         // The number to be encrypted and decrypted (should be string)
-        BigInteger msg = new BigInteger("hi", Character.MAX_RADIX);
+        BigInteger msg = new BigInteger(message.getBytes());
         double c;
         BigInteger msgback;
 
@@ -47,11 +49,14 @@ public class Main {
         // converting float value of c to BigInteger
         BigInteger C = BigDecimal.valueOf(c).toBigInteger();
         msgback = (C.pow(d)).mod(N);
-        System.out.println("Decrypted message is: " + msgback.toString(36));
+        String stringMessage = new String(msgback.toByteArray());
+        System.out.println("Decrypted message is: " + stringMessage);
 
 
     }
 
+
+    //this function exists in a java lib, i think
     static int gcd(int e, int z) {
         if (e == 0)
             return z;
